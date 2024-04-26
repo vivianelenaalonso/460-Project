8bc3f9a756aba7128bd4c16cf2559e8e7b11b6b8
Ifeoluwa Ale <ifeoluwaale@dhcp-10-134-185-223.uawifi.arizona.edu>
2024-04-25 20:22:57 -0700

    Prize queries

--- src/Arcade.java
+++ src/Arcade.java
@@ -1,4 +1,43 @@
+import java.util.Scanner;
+import java.sql.*;
 
 public class Arcade {
+    private static final String PRIZE_TABLE_NAME = "prize_table_name";
+    private static final String ORACLE_URL = 
+                        "jdbc:oracle:thin:@aloe.cs.arizona.edu:1521:oracle";
 
+
+
+    public static void addPrize(Connection dbConn, String prizeId, String prizeName, 
+                                    String ticketAmt) throws SQLException {
+        String query = "INSERT INTO " + PRIZE_TABLE_NAME + """
+        ( 
+            prizeID, name, amount
+        ) VALUES ( """ + 
+            "'" + prizeId.replaceAll("'", "''") + "'," +
+            "'" + prizeName.replaceAll("'", "''") + "'," +
+            ticketAmt.replaceAll("'", "''") +
+        " )";
+
+        Statement stmt = dbConn.createStatement();
+        stmt.executeUpdate(query);
+        stmt.close();
+    }
+
+
+
+    public static void delPrize(Connection dbConn, String prizeId) throws SQLException {
+        String query = "DELETE FROM " + PRIZE_TABLE_NAME + """
+        WHERE prizeID = """ + 
+            "'" + prizeId.replaceAll("'", "''") + "'," +
+        " )";
+
+        Statement stmt = dbConn.createStatement();
+        stmt.executeUpdate(query);
+        stmt.close();
+    }
+
+    public static void main (String[] args) {
+        
+    }
 }
