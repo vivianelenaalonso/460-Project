359fcb1b37b2bec6bbbfcb83c5400bf4846fe7d6
Julram3232 <48529138+NerdRez@users.noreply.github.com>
2024-04-27 04:43:22 -0700

    Added table constants with table names

--- src/Arcade.java
+++ src/Arcade.java
@@ -2,9 +2,19 @@ import java.util.Scanner;
 import java.sql.*;
 
 public class Arcade {
-    private static final String PRIZE_TABLE_NAME = "prize_table_name";
-    private static final String ORACLE_URL = 
-                        "jdbc:oracle:thin:@aloe.cs.arizona.edu:1521:oracle";
+    private static final String ORACLE_URL = "jdbc:oracle:thin:@aloe.cs.arizona.edu:1521:oracle";
+    private static final String ARCADE_GAME_TABLE_NAME = "juliusramirez.ArcadeGame";
+    private static final String BASE_TRANSACTION_TABLE_NAME = "juliusramirez.BaseTransaction";
+    private static final String COUPON_TABLE_NAME = "juliusramirez.Coupon";
+    private static final String COUPON_DETAIL_TABLE_NAME = "juliusramirez.CouponDetail";
+    private static final String COUPON_TRANSACTION_TABLE_NAME = "juliusramirez.CouponTransaction";
+    private static final String GAME_STAT_NAME = "juliusramirez.GameStat";
+    private static final String GAME_TRANSACTION_NAME = "juliusramirez.GameTransaction";
+    private static final String MEMBER_TABLE_NAME = "juliusramirez.Member";
+    private static final String PRIZE_TABLE_NAME = "juliusramirez.Prize";
+    private static final String PRIZE_TRANSACTION_TABLE_NAME = "juliusramirez.PrizeTransaction";
+    private static final String TOKEN_PURCHASE_TIER_TABLE_NAME = "juliusramirez.TokenPurchaseTier";
+    private static final String TOKEN_TRANSACTION_TABLE_NAME = "juliusramirez.TokenTransaction";
 
     public static void addPrize(Connection dbConn, String[] command) throws SQLException {
         String prizeId = command[2];
@@ -12,26 +22,24 @@ public class Arcade {
         String ticketAmt = command[4];
 
         String query = "INSERT INTO " + PRIZE_TABLE_NAME + """
-        ( 
-            prizeID, name, amount
-        ) VALUES ( """ + 
-            "'" + prizeId.replaceAll("'", "''") + "'," +
-            "'" + prizeName.replaceAll("'", "''") + "'," +
-            ticketAmt.replaceAll("'", "''") +
-        " )";
+                (
+                    prizeID, name, amount
+                ) VALUES ( """ +
+                "'" + prizeId.replaceAll("'", "''") + "'," +
+                "'" + prizeName.replaceAll("'", "''") + "'," +
+                ticketAmt.replaceAll("'", "''") +
+                " )";
 
         Statement stmt = dbConn.createStatement();
         stmt.executeUpdate(query);
         stmt.close();
     }
 
-
-
     public static void delPrize(Connection dbConn, String[] command) throws SQLException {
         String prizeId = command[2];
         String query = "DELETE FROM " + PRIZE_TABLE_NAME + """
-        WHERE prizeID = """ + 
-            "'" + prizeId.replaceAll("'", "''") + "'";
+                WHERE prizeID = """ +
+                "'" + prizeId.replaceAll("'", "''") + "'";
 
         Statement stmt = dbConn.createStatement();
         stmt.executeUpdate(query);
@@ -43,7 +51,7 @@ public class Arcade {
         ResultSet answer = null;
 
         String query = "SELECT * FROM " + PRIZE_TABLE_NAME + """
-                WHERE name = """ + 
+                WHERE name = """ +
                 "'" + prizeName.replaceAll("'", "''") + "'";
         Statement stmt = dbConn.createStatement();
         answer = stmt.executeQuery(query);
@@ -53,12 +61,11 @@ public class Arcade {
             // the attribute names and use them as column headers
             ResultSetMetaData answermetadata = answer.getMetaData();
             System.out.println(
-                String.format("%-20s", answermetadata.getColumnName(1))
-                + " | "
-                + String.format("%-20s", answermetadata.getColumnName(2))
-                + " | "
-                + String.format("%-10s", answermetadata.getColumnName(3))
-            );
+                    String.format("%-20s", answermetadata.getColumnName(1))
+                            + " | "
+                            + String.format("%-20s", answermetadata.getColumnName(2))
+                            + " | "
+                            + String.format("%-10s", answermetadata.getColumnName(3)));
             for (int i = 1; i <= 50; i++) {
                 System.out.print("-");
             }
@@ -68,12 +75,11 @@ public class Arcade {
             // tuples and print their attribute values
             while (answer.next()) {
                 System.out.println(
-                    String.format("%-20s", answer.getString("prizeID"))
-                    + " | "
-                    + String.format("%-20s", answer.getString("name"))
-                    + " | "
-                    + String.format("%-10s", answer.getString("amount"))
-                    );
+                        String.format("%-20s", answer.getString("prizeID"))
+                                + " | "
+                                + String.format("%-20s", answer.getString("name"))
+                                + " | "
+                                + String.format("%-10s", answer.getString("amount")));
             }
         }
     }
@@ -84,7 +90,7 @@ public class Arcade {
                 addPrize(dbConn, command);
             } else if (command[1].equalsIgnoreCase("GAME")) {
                 // TODO: addGame
-            } else if (command[1].equalsIgnoreCase("MEMBER")) { 
+            } else if (command[1].equalsIgnoreCase("MEMBER")) {
                 // TODO: addMember
             }
         } else if (command[0].equals("DELETE")) {
@@ -92,7 +98,7 @@ public class Arcade {
                 delPrize(dbConn, command);
             } else if (command[1].equalsIgnoreCase("GAME")) {
                 // TODO: delGame
-            } else if (command[1].equalsIgnoreCase("MEMBER")) { 
+            } else if (command[1].equalsIgnoreCase("MEMBER")) {
                 // TODO: delMember
             }
         } else if (command[0].equals("SEARCH")) {
@@ -100,7 +106,7 @@ public class Arcade {
                 searchPrize(dbConn, command);
             } else if (command[1].equalsIgnoreCase("GAME")) {
                 // TODO: searchGame
-            } else if (command[1].equalsIgnoreCase("MEMBER")) { 
+            } else if (command[1].equalsIgnoreCase("MEMBER")) {
                 // TODO: searchMember
             }
         } else if (command[0].equals("PLAY")) {
@@ -108,7 +114,7 @@ public class Arcade {
         }
     }
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         String username = args[0];
         String password = args[1];
 
@@ -116,18 +122,18 @@ public class Arcade {
             Class.forName("oracle.jdbc.OracleDriver");
         } catch (ClassNotFoundException e) {
             System.err.println("*** ClassNotFoundException:  "
-                + "Error loading Oracle JDBC driver.  \n"
-                + "\tPerhaps the driver is not on the Classpath?");
+                    + "Error loading Oracle JDBC driver.  \n"
+                    + "\tPerhaps the driver is not on the Classpath?");
             System.exit(-1);
         }
 
         Connection dbconn = null;
         try {
-            dbconn = DriverManager.getConnection(ORACLE_URL,username,password);
+            dbconn = DriverManager.getConnection(ORACLE_URL, username, password);
 
         } catch (SQLException e) {
             System.err.println("*** SQLException:  "
-                + "Could not open JDBC connection.");
+                    + "Could not open JDBC connection.");
             System.err.println("\tMessage:   " + e.getMessage());
             System.err.println("\tSQLState:  " + e.getSQLState());
             System.err.println("\tErrorCode: " + e.getErrorCode());
@@ -154,7 +160,7 @@ public class Arcade {
                     processQuery(s.split(" "), dbconn);
                 } catch (SQLException e) {
                     System.err.println("*** SQLException:  "
-                    + "Failed to execute one or more queries.");
+                            + "Failed to execute one or more queries.");
                     System.err.println("\tMessage:   " + e.getMessage());
                     System.err.println("\tSQLState:  " + e.getSQLState());
                     System.err.println("\tErrorCode: " + e.getErrorCode());
